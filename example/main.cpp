31aa5a95ad9bc7ad47a21578bda09528bc680ebf
vas-vas777 <vasya1998@list.ru>
2016-12-01 19:36:31 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -1,48 +1,8 @@
 #include <iostream>
 #include <cstddef>
+#include "calculator.hpp"
 using namespace std;
-float sum(float x, float y)
-{ 
-        return (x + y);
-}
-float mult(float x, float y)
-{ 
-        return (x*y);
-}
-float *dev(float x, float y)
-{ 
-        if (y)        
-        { 
-        float *s=new float; 
-        *s= x/y; 
-        return s;         
-        } 
-        else return nullptr;
-}
-float sub(float x, float y)
-{ 
-        return(x - y);
-}
-float power (float x, float y)
-{      
-      float b; 
-        b=1;         
-          for (int i = 0; i <= y-1; i++)   
-          {     
-                 b = b * x;         
-          } 
-      return b;
-}
-float sq(float x)
-{ 
-      unsigned int div = 1, rslt = 0;
-      while (x > 0) 
-      { 
-              x -= div, div += 2;  
-              rslt += x < 0 ? 0 : 1; 
-      } 
-      return rslt;
-}
+
 int main()
 {    
      float x, y;
